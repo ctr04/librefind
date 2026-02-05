17a4cc207124504e63d125b208f69ea8517486ed
jksalcedo <jks.create@gmail.com>
2026-02-05 11:40:26 +0000

    docs: add changelogs for versions 9 and 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.jksalcedo.librefind"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.0.0-beta9"
+        versionCode = 10
+        versionName = "1.0.0-beta10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
